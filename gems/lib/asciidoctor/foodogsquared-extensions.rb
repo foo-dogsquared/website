047baf8917ac3890c35dea65388e2ccbec00533e
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-03-05 13:48:40 +0800

    Add GitLab raw content include processor

--- gems/lib/asciidoctor/foodogsquared-extensions.rb
+++ gems/lib/asciidoctor/foodogsquared-extensions.rb
@@ -3,9 +3,11 @@ require 'asciidoctor/extensions'
 require_relative 'man-inline-macro/extension'
 require_relative 'swhid-inline-macro/extension'
 require_relative 'github-raw-content-include-processor/extension'
+require_relative 'gitlab-raw-content-include-processor/extension'
 
 Asciidoctor::Extensions.register do
   inline_macro ManInlineMacro
   inline_macro SWHInlineMacro
   include_processor GitHubRawIncludeProcessor
+  include_processor GitLabRawIncludeProcessor
 end
