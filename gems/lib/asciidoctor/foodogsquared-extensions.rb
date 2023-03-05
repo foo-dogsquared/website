d07b784be2fc374b71b20312e5e9e539aff5a4fe
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-03-05 09:47:44 +0800

    Add SWHID link inline macro

--- gems/lib/asciidoctor/foodogsquared-extensions.rb
+++ gems/lib/asciidoctor/foodogsquared-extensions.rb
@@ -1,7 +1,9 @@
 require 'asciidoctor'
 require 'asciidoctor/extensions'
 require_relative 'man-inline-macro/extension'
+require_relative 'swhid-inline-macro/extension'
 
 Asciidoctor::Extensions.register do
   inline_macro ManInlineMacro
+  inline_macro SWHInlineMacro
 end
---
+++ gems/lib/asciidoctor/swhid-inline-macro/extension.rb
@@ -0,0 +1,21 @@
+class SWHInlineMacro < Asciidoctor::Extensions::InlineMacroProcessor
+  use_dsl
+
+  named :swh
+  name_positional_attributes 'caption'
+
+  def process parent, target, attrs
+    doc = parent.document
+
+    # We're only considering `swh:` starting with the scheme version. Also, it
+    # looks nice aesthetically.
+    swhid = (target.start_with? 'swh:') ? target : %(swh:#{target})
+    swhid_core_identifier = (swhid.split ';').at 0
+
+    text = attrs['caption'] || swhid_core_identifier
+    target = %(https://archive.softwareheritage.org/#{swhid})
+
+    doc.register :links, target
+    create_anchor parent, text, type: :link, target: target
+  end
+end
