750d9ba3c7c873c82d7296d632d60bb90649fd5d
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-03-06 23:51:56 +0800

    Update formatting for Asciidoctor extension

--- gems/lib/asciidoctor/foodogsquared-extensions.rb
+++ gems/lib/asciidoctor/foodogsquared-extensions.rb
@@ -2,16 +2,21 @@ require 'asciidoctor'
 require 'asciidoctor/extensions'
 require_relative 'man-inline-macro/extension'
 require_relative 'swhid-inline-macro/extension'
+require_relative 'swhid-include-processor/extension'
 require_relative 'github-link-inline-macro/extension'
 require_relative 'github-raw-content-include-processor/extension'
-require_relative 'gitlab-raw-content-include-processor/extension'
 require_relative 'gitlab-link-inline-macro/extension'
+require_relative 'gitlab-raw-content-include-processor/extension'
 
 Asciidoctor::Extensions.register do
   inline_macro ManInlineMacro
+
   inline_macro SWHInlineMacro
+  include_processor SWHIncludeProcessor
+
   inline_macro GitHubLinkInlineMacro
   include_processor GitHubRawIncludeProcessor
-  include_processor GitLabRawIncludeProcessor
+
   inline_macro GitLabLinkInlineMacro
+  include_processor GitLabRawIncludeProcessor
 end
