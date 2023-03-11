a1800d898ee370a8e9bed3060afadce33638f75c
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-03-11 16:34:12 +0800

    Add SWHID include processor

--- gems/lib/asciidoctor/foodogsquared-extensions.rb
+++ gems/lib/asciidoctor/foodogsquared-extensions.rb
@@ -4,6 +4,7 @@ require 'asciidoctor'
 require 'asciidoctor/extensions'
 require_relative 'man-inline-macro/extension'
 require_relative 'swhid-inline-macro/extension'
+require_relative 'swhid-include-processor/extension'
 require_relative 'github-link-inline-macro/extension'
 require_relative 'github-raw-content-include-processor/extension'
 require_relative 'gitlab-link-inline-macro/extension'
@@ -13,6 +14,7 @@ Asciidoctor::Extensions.register do
   inline_macro ManInlineMacro
 
   inline_macro SWHInlineMacro
+  include_processor SWHIDIncludeProcessor
 
   inline_macro GitHubLinkInlineMacro
   include_processor GitHubRawIncludeProcessor
