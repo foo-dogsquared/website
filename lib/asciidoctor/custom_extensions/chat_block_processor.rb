cbc759a5447febb64a7337b32ff57233d20c916b
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-04-03 23:28:36 +0800

    chat-block-processor: create the initial HTML output

--- lib/asciidoctor/custom_extensions/chat_block_processor.rb
+++ lib/asciidoctor/custom_extensions/chat_block_processor.rb
@@ -8,13 +8,23 @@ class ChatBlock < Asciidoctor::Extensions::BlockProcessor
   name_positional_attributes 'avatar', 'state'
   default_attributes 'state' => 'default', 'avatarstype' => 'webp'
 
-  # TODO: Create the output.
   def process(parent, reader, attrs)
     block = create_block parent, :pass, nil, attrs, content_model: :compound
     block.add_role('dialogblock')
 
     attrs['name'] ||= attrs['avatar']
     attrs['avatarsdir'] ||= './avatars'
+
+    block << (create_block block, :pass, %(
+      <div class="dialogblock dialogblock__box dialogblock__avatar--#{attrs['avatar']} #{attrs['role']}" title="#{attrs['avatar']}">
+        <div class="dialogblock dialogblock__avatar">
+          # Image
+        </div>
+        <div class="dialogblock dialogblock__text">
+          # Content
+        </div>
+      </div>
+    ), nil)
 
     block
   end
