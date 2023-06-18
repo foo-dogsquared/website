5ade8dd12094f911d124cc7626a1108bc2f9f3dc
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-06-18 18:12:59 +0800

    Set cache path for open-uri

--- gems/lib/asciidoctor/foodogsquared-extensions.rb
+++ gems/lib/asciidoctor/foodogsquared-extensions.rb
@@ -2,6 +2,7 @@
 
 require 'asciidoctor'
 require 'asciidoctor/extensions'
+require 'open-uri/cached'
 
 require_relative 'helpers'
 
@@ -20,6 +21,8 @@ require_relative 'fdroid-link-inline-macro/extension'
 require_relative 'musicbrainz-link-inline-macro/extension'
 require_relative 'flathub-link-inline-macro/extension'
 require_relative 'repology-link-inline-macro/extension'
+
+OpenURI::Cache.cache_path = '/tmp/open-uri-cache-foodogsquared-website'
 
 Asciidoctor::Extensions.register do
   inline_macro ManInlineMacro
